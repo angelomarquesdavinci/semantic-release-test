62bbb744977dae08a18f28bea74299fc0a2fc868
Ângelo Marques <angelo@davinciambiental.com.br>
2024-09-11 09:27:42 -0300

    feat: add todays date to page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,12 @@
 export default function HomePage() {
+  const today = new Date().toLocaleDateString();
+
   return (
     <div>
       <h1>Hello, world!</h1>
       <p>Welcome to your new project.</p>
+
+      {today}
     </div>
   );
 }
