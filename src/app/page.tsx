dec9976f5380e992d1487d7f14efed3e9001f352
Ângelo Marques <angelo@davinciambiental.com.br>
2024-09-11 09:30:04 -0300

    feat: add counter

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,9 @@
+import { useState } from "react";
+
 export default function HomePage() {
   const today = new Date().toLocaleDateString();
+
+  const [count, setCount] = useState(0);
 
   return (
     <div>
@@ -7,6 +11,9 @@ export default function HomePage() {
       <p>Welcome to your new project.</p>
 
       {today}
+
+      <p>You clicked {count} times.</p>
+      <button onClick={() => setCount(count + 1)}>Click me</button>
     </div>
   );
 }
